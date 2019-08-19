e73f90486474f80593c06164196fa4672af6f05a
Nahte101 <42877810+Nahte101@users.noreply.github.com>
2019-08-19 13:47:43 +1000

    ScreenOutline and general Classes setup

--- Tetris/Classes/Game1.cs
+++ Tetris/Classes/Game1.cs
@@ -4,13 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Tetris
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        TetrisGameField gameField;
 
         public Game1()
         {
@@ -18,23 +17,13 @@ namespace Tetris
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            gameField = new TetrisGameField(spriteBatch, graphics);
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
@@ -43,20 +32,11 @@ namespace Tetris
             // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -67,15 +47,15 @@ namespace Tetris
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            gameField.drawOutline(spriteBatch);
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ Tetris/Classes/TetrisGameField.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Tetris
+{
+    class TetrisGameField
+    {
+        private static bool[,] I = { { true, false, false, false }, { true, false, false, false }, { true, false, false, false }, { true, false, false, false } };
+        private static bool[,] J = { { true, true, false, false }, { true, false, false, false }, { true, false, false, false }, { false, false, false, false } };
+        private static bool[,] L = { { true, true, false, false }, { false, true, false, false }, { false, true, false, false }, { false, false, false, false } };
+        private static bool[,] O = { { true, true, false, false }, { true, true, false, false }, { false, false, false, false }, { false, false, false, false } };
+        private static bool[,] S = { { true, false, false, false }, { true, true, false, false }, { false, true, false, false }, { false, false, false, false } };
+        private static bool[,] T = { { true, false, false, false }, { true, true, false, false }, { true, false, false, false }, { false, false, false, false } };
+        private static bool[,] Z = { { false, true, false, false }, { true, true, false, false }, { true, false, false, false }, { false, false, false, false } };
+
+        private Texture2D blankRectSprite;
+        
+        private GraphicsDeviceManager graphics;
+        private int screenWidth;
+        private int screenHeight;
+
+        private bool[,] playField = new bool[10, 40];
+        private List<Tetromino> fallenPieces = new List<Tetromino>();
+
+        public TetrisGameField(SpriteBatch spriteBatch, GraphicsDeviceManager graphics)
+        {
+            this.graphics = graphics;//Might be a problem if resolution changes on the fly
+            this.screenHeight = graphics.PreferredBackBufferHeight;
+            this.screenWidth = graphics.PreferredBackBufferWidth;
+
+            
+        }
+        public void drawOutline(SpriteBatch spriteBatch)
+        {
+            blankRectSprite = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
+            blankRectSprite.SetData(new Color[] { Color.GhostWhite });
+
+            //Variables for usage in drawing of box (playing field will be 10 by 40 blocks default same as playfield)
+            // and each block will be 10 by 10 pixels with a 5 block gap on the x and y
+
+            int widthInBlocks = playField.GetLength(0);
+            int heightInBlocks = playField.GetLength(1);
+            int screenWidth = graphics.PreferredBackBufferWidth;
+            int screenHeight = graphics.PreferredBackBufferHeight;
+            int blockSize = 10;
+            int gap = 5;
+
+            //Horizontal Top Line
+            spriteBatch.Draw(blankRectSprite, new Rectangle( (screenWidth/2 )-( ( widthInBlocks* (blockSize+gap) ) /2)
+            ,(screenHeight/16), widthInBlocks * (blockSize + gap), 1 ), Color.White);
+            //Horizontal Bottom Line
+            spriteBatch.Draw(blankRectSprite, new Rectangle((screenWidth / 2) - ((widthInBlocks * (blockSize + gap)) / 2)
+            , screenHeight- (screenHeight / 16), widthInBlocks * (blockSize + gap), 1), Color.White);
+            //Vertical Left Line
+            spriteBatch.Draw(blankRectSprite, new Rectangle( (screenWidth/2)-( (widthInBlocks * (blockSize + gap) ) /2 ) 
+                , (screenHeight / 16), 1,screenHeight-( 2 * (screenHeight/16) ) ), Color.White);
+            //Vertical Right Line
+            spriteBatch.Draw(blankRectSprite, new Rectangle((screenWidth / 2) + ((widthInBlocks * (blockSize + gap)) / 2)
+                , (screenHeight / 16), 1, screenHeight - (2 * (screenHeight / 16))), Color.White);
+        }
+    }
+}
